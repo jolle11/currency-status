f0c2e069ac91c65ee562988e99f86d4161280a39
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-11-03 21:56:56 +0100

    Added title and navigation menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import './Header.scss';
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div>
+            <h1>Curr€nc¥ &nbsp;$tatus</h1>
+            <ul>
+                <li>
+                    <Link to="/">Global Conversion</Link>
+                </li>
+                <li>
+                    <Link to="/specificConversion">Specific Conversion</Link>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default Header;
