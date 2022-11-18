f27cb86fed98ec1a92e9f773157a67f8ade4b15a
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-11-18 13:10:54 +0100

    Fixed negative numbers

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,134 +1,144 @@
-import axios, { AxiosResponse } from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
 
-import { CurrencyCard, Header, Footer } from '../../components';
+import { CurrencyCard, Header, Footer } from "../../components";
 
-import './Home.scss';
+import "./Home.scss";
 
-import currenciesObject from '../../utils/currenciesObject';
+import currenciesObject from "../../utils/currenciesObject";
 
 interface SelectedCurrency {
-    flag: string;
-    acronym: string;
-    name?: string;
-    rate?: number;
+	flag: string;
+	acronym: string;
+	name?: string;
+	rate?: number;
 }
 
 interface Rates {
-    rate: number;
+	rate: number;
 }
 
-const apiUrl: string = 'https://api.frankfurter.app/latest';
+const apiUrl: string = "https://api.frankfurter.app/latest";
 
 const Home = () => {
-    const [currencies, setCurrencies] = useState<SelectedCurrency[]>(currenciesObject);
-    const [selectedCurrency, setSelectedCurrency] = useState<SelectedCurrency>();
-    const [currencyList, setCurrencyList] = useState<SelectedCurrency[]>([]);
-    const [rates, setRates] = useState<Rates[]>([]);
-    const [inputNumber, setInputNumber] = useState<number>(0);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [currencies, setCurrencies] =
+		useState<SelectedCurrency[]>(currenciesObject);
+	const [selectedCurrency, setSelectedCurrency] = useState<SelectedCurrency>();
+	const [currencyList, setCurrencyList] = useState<SelectedCurrency[]>([]);
+	const [rates, setRates] = useState<Rates[]>([]);
+	const [inputNumber, setInputNumber] = useState<number>(0);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    const handleInputChange = (e: React.ChangeEvent) => {
-        const limit: number = 11;
-        setInputNumber(Number((e.target as HTMLInputElement).value.slice(0, limit)));
-    };
+	const handleInputChange = (e: React.ChangeEvent) => {
+		const limit: number = 11;
+		setInputNumber(
+			Number((e.target as HTMLInputElement).value.slice(0, limit)),
+		);
+	};
 
-    const handleCurrencySelect = (currency: object, index: number) => {
-        setIsLoading(true);
-        setSelectedCurrency(currencies[index]);
-        const selectedCurrencyExcluded: SelectedCurrency[] = currencies.filter((selected) => {
-            return selected !== currency;
-        });
-        setCurrencyList(selectedCurrencyExcluded);
-        setInputNumber(1);
-        window.scrollTo(0, 0);
-        // localStorage.setItem('currency', JSON.stringify(currency));
-    };
+	const handleCurrencySelect = (currency: object, index: number) => {
+		setIsLoading(true);
+		setSelectedCurrency(currencies[index]);
+		const selectedCurrencyExcluded: SelectedCurrency[] = currencies.filter(
+			(selected) => {
+				return selected !== currency;
+			},
+		);
+		setCurrencyList(selectedCurrencyExcluded);
+		setInputNumber(1);
+		window.scrollTo(0, 0);
+		// localStorage.setItem('currency', JSON.stringify(currency));
+	};
 
-    const handleCurrencyChange = (currency: SelectedCurrency) => {
-        setIsLoading(true);
-        setSelectedCurrency(currency);
-        const changeCurrencyExcluded: SelectedCurrency[] = currencies.filter((selected) => {
-            return selected.acronym !== currency.acronym;
-        });
-        setCurrencyList(changeCurrencyExcluded);
-        window.scrollTo(0, 0);
-        // localStorage.setItem('currency', JSON.stringify(currency));
-    };
+	const handleCurrencyChange = (currency: SelectedCurrency) => {
+		setIsLoading(true);
+		setSelectedCurrency(currency);
+		const changeCurrencyExcluded: SelectedCurrency[] = currencies.filter(
+			(selected) => {
+				return selected.acronym !== currency.acronym;
+			},
+		);
+		setCurrencyList(changeCurrencyExcluded);
+		window.scrollTo(0, 0);
+		// localStorage.setItem('currency', JSON.stringify(currency));
+	};
 
-    useEffect(() => {
-        // TODO Get currency del localStorage
-        // TODO Set selectedCurrency
-        axios
-            .get(
-                `${apiUrl}?from=${
-                    selectedCurrency?.acronym !== undefined ? selectedCurrency.acronym : 'EUR'
-                }`
-            )
-            .then((response: AxiosResponse) => {
-                const ratesObject: Rates = response.data.rates;
-                // Create array of rate objects
-                let actualRates: Rates[] = [];
-                Object.entries(ratesObject).forEach(([key, value]) => {
-                    actualRates.push({ rate: value });
-                });
-                setRates(actualRates);
-                setIsLoading(false);
-            })
-            .catch((error: any) => {
-                console.log(error);
-            });
-    }, [selectedCurrency, setSelectedCurrency]);
+	useEffect(() => {
+		// TODO Get currency del localStorage
+		// TODO Set selectedCurrency
+		axios
+			.get(
+				`${apiUrl}?from=${
+					selectedCurrency?.acronym !== undefined
+						? selectedCurrency.acronym
+						: "EUR"
+				}`,
+			)
+			.then((response: AxiosResponse) => {
+				const ratesObject: Rates = response.data.rates;
+				// Create array of rate objects
+				let actualRates: Rates[] = [];
+				Object.entries(ratesObject).forEach(([key, value]) => {
+					actualRates.push({ rate: value });
+				});
+				setRates(actualRates);
+				setIsLoading(false);
+			})
+			.catch((error: any) => {
+				console.log(error);
+			});
+	}, [selectedCurrency, setSelectedCurrency]);
 
-    return (
-        <>
-            <Header />
-            {selectedCurrency ? (
-                <div className="currency">
-                    <form className="currency__form">
-                        <h1 className="currency__title">{selectedCurrency.flag}</h1>
-                        <label className="currency__selected">
-                            {selectedCurrency.acronym} - {selectedCurrency.name}
-                        </label>
-                        <br />
-                        <input
-                            type="number"
-                            name="currency"
-                            value={inputNumber}
-                            placeholder="0"
-                            onChange={handleInputChange}
-                            className="currency__input"
-                        />
-                    </form>
-                    {currencyList.map((currency, index) => (
-                        <div key={index} className="currency__list">
-                            <CurrencyCard
-                                currency={currency}
-                                index={index}
-                                rates={rates}
-                                inputNumber={inputNumber}
-                                handleCurrencyChange={handleCurrencyChange}
-                                isLoading={isLoading}
-                            />
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <div className="currency">
-                    {currencies.map((currency, index) => (
-                        <button
-                            key={index}
-                            className="currency__select"
-                            onClick={() => handleCurrencySelect(currency, index)}
-                        >
-                            {currency.flag}
-                        </button>
-                    ))}
-                </div>
-            )}
-            <Footer />
-        </>
-    );
+	return (
+		<>
+			<Header />
+			{selectedCurrency ? (
+				<div className="currency">
+					<form className="currency__form">
+						<h1 className="currency__title">{selectedCurrency.flag}</h1>
+						<label className="currency__selected">
+							{selectedCurrency.acronym} - {selectedCurrency.name}
+						</label>
+						<br />
+						<input
+							type="number"
+							name="currency"
+							min='0'
+							value={inputNumber}
+							placeholder="0"
+							onChange={handleInputChange}
+							className="currency__input"
+						/>
+					</form>
+					{currencyList.map((currency, index) => (
+						<div key={index} className="currency__list">
+							<CurrencyCard
+								currency={currency}
+								index={index}
+								rates={rates}
+								inputNumber={inputNumber}
+								handleCurrencyChange={handleCurrencyChange}
+								isLoading={isLoading}
+							/>
+						</div>
+					))}
+				</div>
+			) : (
+				<div className="currency">
+					{currencies.map((currency, index) => (
+						<button
+							key={index}
+							className="currency__select"
+							onClick={() => handleCurrencySelect(currency, index)}
+						>
+							{currency.flag}
+						</button>
+					))}
+				</div>
+			)}
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
--- src/pages/SpecificConversion/SpecificConversion.tsx
+++ src/pages/SpecificConversion/SpecificConversion.tsx
@@ -1,141 +1,150 @@
-import axios, { AxiosResponse } from 'axios';
-import React, { useEffect, useState } from 'react';
-import { toast } from 'react-toastify';
+import axios, { AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-import { AiOutlineCopy } from 'react-icons/ai';
+import { AiOutlineCopy } from "react-icons/ai";
 
-import { Header, Footer } from '../../components';
+import { Header, Footer } from "../../components";
 
-import currenciesObject from '../../utils/currenciesObject';
+import currenciesObject from "../../utils/currenciesObject";
 
-import './SpecificConversion.scss';
+import "./SpecificConversion.scss";
 
 interface Rate {
-    rate: number;
+	rate: number;
 }
 
-const apiUrl: string = 'https://api.frankfurter.app/latest?amount=1&from=';
+const apiUrl: string = "https://api.frankfurter.app/latest?amount=1&from=";
 
 const SpecificConversion = () => {
-    const [currencyFrom, setCurrencyFrom] = useState<number>(1);
-    const [selectedCurrencyFrom, setSelectedCurrencyFrom] = useState<string>('AUD');
-    const [currencyTo, setCurrencyTo] = useState<number>(1);
-    const [selectedCurrencyTo, setSelectedCurrencyTo] = useState<string>('AUD');
-    const [rate, setRate] = useState<Rate>({ rate: 1 });
+	const [currencyFrom, setCurrencyFrom] = useState<number>(1);
+	const [selectedCurrencyFrom, setSelectedCurrencyFrom] =
+		useState<string>("AUD");
+	const [currencyTo, setCurrencyTo] = useState<number>(1);
+	const [selectedCurrencyTo, setSelectedCurrencyTo] = useState<string>("AUD");
+	const [rate, setRate] = useState<Rate>({ rate: 1 });
 
-    const handleInputChange = (e: React.ChangeEvent) => {
-        const limit: number = 11;
-        if (selectedCurrencyFrom === selectedCurrencyTo) {
-            setRate({ rate: 1 });
-        }
-        setCurrencyFrom(Number((e.target as HTMLInputElement).value.slice(0, limit)));
-        setCurrencyTo(Number((e.target as HTMLInputElement).value) * rate.rate);
-    };
+	const handleInputChange = (e: React.ChangeEvent) => {
+		const limit: number = 11;
+		if (selectedCurrencyFrom === selectedCurrencyTo) {
+			setRate({ rate: 1 });
+		}
+		setCurrencyFrom(
+			Number((e.target as HTMLInputElement).value.slice(0, limit)),
+		);
+		setCurrencyTo(Number((e.target as HTMLInputElement).value) * rate.rate);
+	};
 
-    const handleFromSelectChange = (e: React.ChangeEvent) => {
-        if (selectedCurrencyFrom === selectedCurrencyTo) {
-            setRate({ rate: 1 });
-        }
-        setSelectedCurrencyFrom((e.target as HTMLInputElement).value);
-    };
+	const handleFromSelectChange = (e: React.ChangeEvent) => {
+		if (selectedCurrencyFrom === selectedCurrencyTo) {
+			setRate({ rate: 1 });
+		}
+		setSelectedCurrencyFrom((e.target as HTMLInputElement).value);
+	};
 
-    const handleToSelectChange = (e: React.ChangeEvent) => {
-        if (selectedCurrencyFrom === selectedCurrencyTo) {
-            setRate({ rate: 1 });
-        }
-        setSelectedCurrencyTo((e.target as HTMLInputElement).value);
-    };
+	const handleToSelectChange = (e: React.ChangeEvent) => {
+		if (selectedCurrencyFrom === selectedCurrencyTo) {
+			setRate({ rate: 1 });
+		}
+		setSelectedCurrencyTo((e.target as HTMLInputElement).value);
+	};
 
-    const copyConversion = () => {
-        navigator.clipboard.writeText(
-            `${currencyFrom} ${selectedCurrencyFrom} = ${currencyTo} ${selectedCurrencyTo}`
-        );
-        toast('Conversion copied to the clipboard 📋', {
-            position: 'top-center',
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        });
-    };
+	const copyConversion = () => {
+		navigator.clipboard.writeText(
+			`${currencyFrom} ${selectedCurrencyFrom} = ${currencyTo} ${selectedCurrencyTo}`,
+		);
+		toast("Conversion copied to the clipboard 📋", {
+			position: "top-center",
+			autoClose: 2000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+		});
+	};
 
-    useEffect(() => {
-        // TODO Get currency del localStorage
-        // TODO Set selectedCurrency
-        if (selectedCurrencyFrom !== selectedCurrencyTo) {
-            axios
-                .get(`${apiUrl}${selectedCurrencyFrom}&to=${selectedCurrencyTo}`)
-                .then((response: AxiosResponse) => {
-                    let rateResponse: object = response.data.rates;
-                    let responseValue: number = Object.values(rateResponse)[0];
-                    setRate({ rate: responseValue });
-                    setCurrencyTo(currencyFrom * responseValue);
-                })
-                .catch((error: any) => {
-                    console.log(error);
-                });
-        }
-    }, [selectedCurrencyFrom, setSelectedCurrencyFrom, selectedCurrencyTo, setSelectedCurrencyTo]);
+	useEffect(() => {
+		// TODO Get currency del localStorage
+		// TODO Set selectedCurrency
+		if (selectedCurrencyFrom !== selectedCurrencyTo) {
+			axios
+				.get(`${apiUrl}${selectedCurrencyFrom}&to=${selectedCurrencyTo}`)
+				.then((response: AxiosResponse) => {
+					let rateResponse: object = response.data.rates;
+					let responseValue: number = Object.values(rateResponse)[0];
+					setRate({ rate: responseValue });
+					setCurrencyTo(currencyFrom * responseValue);
+				})
+				.catch((error: any) => {
+					console.log(error);
+				});
+		}
+	}, [
+		selectedCurrencyFrom,
+		setSelectedCurrencyFrom,
+		selectedCurrencyTo,
+		setSelectedCurrencyTo,
+	]);
 
-    return (
-        <>
-            <Header />
-            <div className="specific">
-                <h1 className="specific__title">$pecific Conv€rsion</h1>
-                <form className="specific__form">
-                    <div className="specific__from">
-                        <label htmlFor="From" className="specific__label">
-                            From:
-                        </label>
-                        <select
-                            name="currencyFrom"
-                            onChange={handleFromSelectChange}
-                            className="specific__select"
-                        >
-                            {currenciesObject.map((currency) => (
-                                <option key={currency.acronym} value={currency.acronym}>
-                                    {currency.flag} {currency.acronym}
-                                </option>
-                            ))}
-                        </select>
-                        <input
-                            type="number"
-                            name="from"
-                            placeholder="0"
-                            className="specific__input"
-                            value={currencyFrom}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-                    <span className="specific__icon" onClick={copyConversion}>
-                        <AiOutlineCopy />
-                    </span>
-                    <div className="specific__to">
-                        <label htmlFor="To" className="specific__label">
-                            To:
-                        </label>
-                        <select
-                            name="currencyTo"
-                            onChange={handleToSelectChange}
-                            className="specific__select"
-                        >
-                            {currenciesObject.map((currency) => (
-                                <option key={currency.acronym} value={currency.acronym}>
-                                    {currency.flag} {currency.acronym}
-                                </option>
-                            ))}
-                        </select>
-                        <p className="specific__result">
-                            {Math.round((currencyTo + Number.EPSILON) * 100) / 100}
-                        </p>
-                    </div>
-                </form>
-            </div>
-            <Footer />
-        </>
-    );
+	return (
+		<>
+			<Header />
+			<div className="specific">
+				<h1 className="specific__title">$pecific Conv€rsion</h1>
+				<form className="specific__form">
+					<div className="specific__from">
+						<label htmlFor="From" className="specific__label">
+							From:
+						</label>
+						<select
+							name="currencyFrom"
+							onChange={handleFromSelectChange}
+							className="specific__select"
+						>
+							{currenciesObject.map((currency) => (
+								<option key={currency.acronym} value={currency.acronym}>
+									{currency.flag} {currency.acronym}
+								</option>
+							))}
+						</select>
+						<input
+							type="number"
+							name="from"
+							min='0'
+							placeholder="0"
+							className="specific__input"
+							value={currencyFrom}
+							onChange={handleInputChange}
+						/>
+					</div>
+					<span className="specific__icon" onClick={copyConversion}>
+						<AiOutlineCopy />
+					</span>
+					<div className="specific__to">
+						<label htmlFor="To" className="specific__label">
+							To:
+						</label>
+						<select
+							name="currencyTo"
+							onChange={handleToSelectChange}
+							className="specific__select"
+						>
+							{currenciesObject.map((currency) => (
+								<option key={currency.acronym} value={currency.acronym}>
+									{currency.flag} {currency.acronym}
+								</option>
+							))}
+						</select>
+						<p className="specific__result">
+							{Math.round((currencyTo + Number.EPSILON) * 100) / 100}
+						</p>
+					</div>
+				</form>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default SpecificConversion;
