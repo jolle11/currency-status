3cd3e6013b2765b9a3cebea0eef421fce7ee5955
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-12-11 20:31:44 +0100

    feat: add x icon, update to vite 5 and refactor footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,45 @@
 import { AiFillInstagram } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
 import { SiHashnode } from "react-icons/si";
-import { VscGithubInverted, VscTwitter } from "react-icons/vsc";
+import { VscGithubInverted } from "react-icons/vsc";
 
 import "./Footer.scss";
 
+interface SocialLink {
+    href: string;
+    icon: JSX.Element;
+    className: string;
+}
+
 const Footer = () => {
+    const socialLinks: SocialLink[] = [
+        {
+            href: "https://github.com/jolle11",
+            icon: <VscGithubInverted />,
+            className: "footer__link"
+        },
+        {
+            href: "https://www.linkedin.com/in/jordi-oll%C3%A9-ballest%C3%A9-8398b181/",
+            icon: <FaLinkedinIn />,
+            className: "footer__link footer__link--linkedin"
+        },
+        {
+            href: "https://jordi0lle.hashnode.dev/",
+            icon: <SiHashnode />,
+            className: "footer__link footer__link--hashnode"
+        },
+        {
+            href: "https://twitter.com/jordi0lle",
+            icon: <FaXTwitter />,
+            className: "footer__link footer__link--twitter"
+        },
+        {
+            href: "https://www.instagram.com/jordi0lle/",
+            icon: <AiFillInstagram />,
+            className: "footer__link footer__link--instagram"
+        }
+    ];
+
     return (
         <div className="footer">
             <p className="footer__text">Created by Jordi Ollé</p>
@@ -19,33 +53,17 @@ const Footer = () => {
                 </a>
             </p>
             <div className="footer__links">
-                <a href="https://github.com/jolle11" className="footer__link">
-                    <VscGithubInverted />
-                </a>
-                <a
-                    href="https://www.linkedin.com/in/jordi-oll%C3%A9-ballest%C3%A9-8398b181/"
-                    className="footer__link footer__link--linkedin"
-                >
-                    <FaLinkedinIn />
-                </a>
-                <a
-                    href="https://jordi0lle.hashnode.dev/"
-                    className="footer__link footer__link--hashnode"
-                >
-                    <SiHashnode />
-                </a>
-                <a
-                    href="https://twitter.com/jordi0lle"
-                    className="footer__link footer__link--twitter"
-                >
-                    <VscTwitter />
-                </a>
-                <a
-                    href="https://www.instagram.com/jordi0lle/"
-                    className="footer__link footer__link--instagram"
-                >
-                    <AiFillInstagram />
-                </a>
+                {socialLinks.map((link, index) => (
+                    <a
+                        key={index}
+                        href={link.href}
+                        className={link.className}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {link.icon}
+                    </a>
+                ))}
             </div>
         </div>
     );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react';
+import Fonts from 'unplugin-fonts/vite';
 import { defineConfig } from 'vite';
-import { VitePluginFonts } from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		VitePluginFonts({
+		Fonts({
 			google: {
 				families: [
 					{
